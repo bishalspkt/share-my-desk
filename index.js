a89916ee890cb46ae70d2efded61d2d79ced1fa3
Bishal Sapkota <bishalspkt@gmail.com>
2018-10-24 23:13:09 +1100

    Add a server module which encapsulates Express

--- index.js
+++ index.js
@@ -3,19 +3,37 @@
 const async = require("async");
 const utils = require("./lib/utils");
 const logger = require("./lib/logger");
+const server = require("./lib/server").create();
+const ramlSpecPath = "raml/api.raml";
 
 utils.assertNodeRuntime();
 
-const installRAML = (cb) => {
-    cb(null);
+const installRAMLMiddleware = (done) => {
+    server.installRAML(ramlSpecPath, (err, server) => done(err, server));
 };
 
-async.series([
-    installRAML
+const startListening = (server, done) => {
+    server.listen(done);
+};
+
+const installApiDocWs = (server, done) => {
+    server.installApiDocWs(ramlSpecPath);
+    done(null, server);
+};
+
+const installWebServices = (server, done) => {
+    // None built so far
+    done(null, server);
+};
+
+async.waterfall([
+    installRAMLMiddleware,
+    installApiDocWs,
+    installWebServices,
+    startListening
 ], (err) => {
     if (err) {
-        logger.error(err.message);
-        logger.error(err.stack);
+        logger.error(err);
         process.exit(1);
     }
 });
