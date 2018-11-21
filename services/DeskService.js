58b5205a5d139d49c8a2f4aeab7d9ea4a9d77c86
Bishal Sapkota <bishalspkt@gmail.com>
2018-11-21 21:17:34 +1100

    Refactor date login into functions

--- services/DeskService.js
+++ services/DeskService.js
@@ -6,8 +6,40 @@ const utils = require("../lib/utils");
 const errcodes = require("../lib/constants").ErrorCodes();
 const officeLocations = require("./OfficeLocationsService").getOfficeLocations();
 
-// How many days into the future can a desk be shared. 
+// How many days into the future can a desk be shared.
 const canShareForDaysInFuture = utils.getConfig("CAN_SHARE_FOR_DAYS_IN_FUTURE", 60);
+
+/**
+ * parses a string for a valid date
+ * @param {String} dateString of format YYYYMMDD
+ * @returns {Moment} date or null
+ */
+const parseDateString = (dateString) => {
+    var momentDate = moment(dateString, "YYYYMMDD");
+    return momentDate.isValid() ? momentDate : null;
+};
+/**
+ * Check if the moment date is before today
+ * @param {Moment} dateToTest
+ * @returns {Boolean}
+ */
+const isDateBeforeToday = (dateToTest) => dateToTest.isBefore(moment().startOf("day"));
+
+/**
+ * Check is the date is within the last allowed day in the future
+ * @param {Moment} dateToTest
+ * @returns {Boolean}
+ */
+const isDateWithinAllowedFuture = (dateToTest) => dateToTest.isBefore(moment().startOf("day").add(canShareForDaysInFuture + 1, "days"));
+
+/**
+ * Check is the date is within the date ranges allowed
+ * Between today and last allowed day in the future
+ * @param {Moment} dateToTest
+ * @returns {Boolean}
+ */
+const isDateInValidRange = (dateToTest) => !isDateBeforeToday(dateToTest) && isDateWithinAllowedFuture(dateToTest);
+
 
 exports.DeskService = (availableDeskModel, userModel) => {
     if (utils.isNull(availableDeskModel)) {
@@ -17,24 +49,23 @@ exports.DeskService = (availableDeskModel, userModel) => {
         userModel = require("../models/User");
     }
 
-    const instance = {}
+    const instance = {};
 
     instance.share = (deskDetails, userId, cb) => {
         // Check if known officeLocation is passed in
-        if(officeLocations.map(office => office.location).indexOf(deskDetails.officeLocation) == -1) {
+        if (officeLocations.map(office => office.location).indexOf(deskDetails.officeLocation) == -1) {
             return cb(errcodes.INVALID_OFFICE_LOCATION);
         }
 
         // Check if the dates being passed in were valid
-        const today = moment().startOf('day');
-        const lastValidDay = today.clone().add(canShareForDaysInFuture, 'days'); // add mutates the original value, so clone() it beforehand
         for (var i = 0; i < deskDetails.datesAvailable.length; i++) {
-            var momentDate = moment(deskDetails.datesAvailable[i], "YYYYMMDD");
-            if (!momentDate.isValid()) {
+
+            var momentDate = parseDateString(deskDetails.datesAvailable[i]);
+            if (utils.isNull(momentDate)) {
                 return cb(errcodes.INVALID_DATE_FORMAT);
             }
 
-            if (momentDate.isBefore(today) || momentDate.isAfter(lastValidDay)) {
+            if (!isDateInValidRange(momentDate)) {
                 return cb(errcodes.DATE_NOT_IN_VALID_RANGE);
             }
         }
@@ -65,56 +96,55 @@ exports.DeskService = (availableDeskModel, userModel) => {
                     directions: deskDetails.directions,
                     closestRoomName: deskDetails.closestRoomName,
                     postedBy: postedBy
-                }
-            })
+                };
+            });
             availableDeskModel.insertMany(insertObjects, (err, docs) => {
                 if (utils.isNotNull(err)) {
                     return cb(errcodes.DATABASE_ERROR);
                 }
                 return cb(null, {insertCount: docs.length});
-            })
+            });
         });
-        
+
 
     };
 
     instance.getDesks = (query, cb) => {
         // Validate query parameters
-        if(officeLocations.map(office => office.location).indexOf(query.officeLocation) == -1) {
+        if (officeLocations.map(office => office.location).indexOf(query.officeLocation) == -1) {
             return cb(errcodes.INVALID_OFFICE_LOCATION);
         }
 
-        var momentDate = moment(query.date, "YYYYMMDD");
-        if (!momentDate.isValid()) {
+        var momentDate = parseDateString(query.date);
+        if (utils.isNull(momentDate)) {
             return cb(errcodes.INVALID_DATE_FORMAT);
         }
-        const today = moment().startOf('day');
-        if(momentDate.isBefore(today)) {
+        if (isDateBeforeToday(momentDate)) {
             return cb(errcodes.DATE_NOT_IN_VALID_RANGE);
         }
         // TODO: Determine how farther into the future are searches allowed.
 
         availableDeskModel.
             find(query).
-            populate('postedBy').
+            populate("postedBy").
             exec((err, _desks) => {
-            if(err) {
-                return cb(errcodes.DATABASE_ERROR);
-            }
+                if (err) {
+                    return cb(errcodes.DATABASE_ERROR);
+                }
 
-            if(utils.isEmpty(_desks)) {
-                return cb(errcodes.NO_MATCH_FOUND);
-            }
+                if (utils.isEmpty(_desks)) {
+                    return cb(errcodes.NO_MATCH_FOUND);
+                }
 
-            // Return only the relevant information
-            let desks = createDeskResponse(_desks);
-            cb(null, { desks: desks });
-        });
+                // Return only the relevant information
+                let desks = createDeskResponse(_desks);
+                cb(null, { desks: desks });
+            });
     };
 
     /**
      * Maps mongoose returned raw desks array to minimal desk details needed by the client
-     * @param {[AvailableDesk]} mongooseDesks returned from database 
+     * @param {[AvailableDesk]} mongooseDesks returned from database
      */
     const createDeskResponse = (mongooseDesks) => {
         return mongooseDesks.map(_desk => {
@@ -133,8 +163,14 @@ exports.DeskService = (availableDeskModel, userModel) => {
                 },
                 postedOn: _desk.updatedAt
 
-            }
+            };
         });
-    }
+    };
     return instance;
-}
+};
+
+// Exporting for testability
+exports.parseDateString = parseDateString;
+exports.isDateBeforeToday = isDateBeforeToday;
+exports.isDateWithinAllowedFuture = isDateWithinAllowedFuture;
+exports.isDateInValidRange = isDateInValidRange;
